57bc2b5c193414194168e20e7dadc386d3e5d04d
tiger4th <tiger4thg@gmail.com>
2018-03-04 19:40:10 +0900

    VIPSTAR仕様に修正

--- verum/js/main.js
+++ verum/js/main.js
@@ -3,7 +3,7 @@ function main() {
 
 (function () {
    'use strict';
-   
+
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -22,7 +22,7 @@ $('#nav').affix({
       offset: {
         top: $('header').height()
       }
-});	
+});
 
 	// skills chart
 	$(document).ready(function(e) {
@@ -32,15 +32,15 @@ $('#nav').affix({
 		var top = $('#skills').height()-$(window).scrollTop();
 		console.log(top)
 		if(top<-300){
-			if(index==0){	
-			
+			if(index==0){
+
 				$('.chart').easyPieChart({
 					easing: 'easeOutBounce',
 					onStep: function(from, to, percent) {
 						$(this.el).find('.percent').text(Math.round(percent));
 					}
 				});
-			
+
 				}
 			index++;
 		}
@@ -51,7 +51,7 @@ $('#nav').affix({
 
   	// Portfolio isotope filter
     $(window).load(function() {
-        var $container = $('.portfolio-items');
+        var $container = $('.event-items');
         $container.isotope({
             filter: '*',
             animationOptions: {
@@ -76,22 +76,22 @@ $('#nav').affix({
         });
 
     });
-	
-	  	
+
+
     // CounterUp
 	$(document).ready(function( $ ) {
-		if($("span.count").length > 0){	
+		if($("span.count").length > 0){
 			$('span.count').counterUp({
 					delay: 10, // the delay time in ms
 			time: 1500 // the speed time in ms
 			});
 		}
 	});
-	
+
   	// Pretty Photo
 	$("a[rel^='prettyPhoto']").prettyPhoto({
 		social_tools: false
-	});	
+	});
 
 }());
 
