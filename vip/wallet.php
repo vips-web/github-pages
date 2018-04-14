775010af9c49c64c691ed186b6f1f8b48419575b
vips-web <tiger4thv@gmail.com>
2018-04-15 00:57:13 +0900

    ウォレットリンク追加・修正

--- vip/wallet.php
+++ vip/wallet.php
@@ -8,11 +8,17 @@
     <div class="row">
       <div class="col-md-8 col-md-offset-2">
         <div class="wallet-button">
-          <a href="http://vipsvips.sakura.ne.jp/download/VIPSTARCOIN_win_normal.zip">
+          <a href="https://github.com/vips-web/github-pages/raw/master/download/VIPSTARCOIN_win_normal.zip">
             <img src="<?php echo URL; ?>/img/wallet/win-normal.png" alt="" width="161" height="51">
-          </a><br>
-          <a href="http://vipsvips.sakura.ne.jp/download/VIPSTARCOIN_mac_normal.zip">
+          </a>
+          <a href="https://github.com/vips-web/github-pages/raw/master/download/VIPSTARCOIN_mac_normal.zip">
             <img src="<?php echo URL; ?>/img/wallet/mac-normal.png" alt="" width="161" height="51">
+          </a><br>
+          <a href="https://github.com/vips-web/github-pages/raw/master/download/VIPSTARCOIN_win_ms_vips.zip">
+            <img src="<?php echo URL; ?>/img/wallet/win-ms-vips.png" alt="" width="161" height="51">
+          </a>
+          <a href="https://github.com/vips-web/github-pages/raw/master/download/VIPSTARCOIN_mac_ms_vips.zip">
+            <img src="<?php echo URL; ?>/img/wallet/mac-ms-vips.png" alt="" width="161" height="51">
           </a><br>
           <a href="http://explorer.vipstarcoin.jp/" class="button button-bw" target="_blank">
             VIPSTARCOIN Explorer&nbsp;&nbsp;<i class="fas fa-external-link-alt"></i>
