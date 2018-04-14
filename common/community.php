ef451a716dcc015289ebd2237647eb2518745cb3
vips-web <tiger4thv@gmail.com>
2018-04-15 01:21:28 +0900

    community修正

--- common/community.php
+++ common/community.php
@@ -10,7 +10,7 @@
         <a href="https://discordapp.com/invite/K5gPZ2h" target="_blank">
           <div class="community-box">
             <i class="fab fa-discord fa-5x"></i>
-            <span class="count">3172</span>
+            <span class="count">4107</span>
             <h4>Members</h4>
           </div>
         </a>
@@ -19,7 +19,7 @@
         <a href="https://twitter.com/VIPSTARCOIN" target="_blank">
           <div class="community-box">
             <i class="fab fa-twitter fa-5x"></i>
-            <span class="count">2726</span>
+            <span class="count">3314</span>
             <h4>Followers</h4>
           </div>
         </a>
