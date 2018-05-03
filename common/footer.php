c2a7dea44c952125ab8f89eeac6fed3cae149d32
vips-web <tiger4thv@gmail.com>
2018-05-03 22:47:47 +0900

    footer修正

--- common/footer.php
+++ common/footer.php
@@ -4,10 +4,10 @@
       <div class="addthis_inline_share_toolbox"></div>
       <p>
         <?php if ($lang == 'ja'): ?>
-          ホームページ開発&amp;デザイン tiger: VLvHafAdfzP85pNLfGw9dCveL5RPj3YpSo<br>
+          ホームページ開発&amp;デザイン&amp;運用 tiger: VLvHafAdfzP85pNLfGw9dCveL5RPj3YpSo<br>
           ホワイトペーパーデザイン協力 ぼおん-qiv-: VBPakpHTXSS1fuRj9i3xReec579dfB9Lf9
         <?php elseif ($lang == 'de'): ?>
-          Adresse des Übersetzers: VUu91vN2LRF5nCpyVbrrt46PTbT5SSXHkp<br>
+          Adresse des Übersetzers: VUhAW7TK3Eqc79rYwPrUimt1KHuzzJfFtD<br>
           Dankeschön-VIPS werden 24/7 angenommen!
         <?php elseif ($lang == 'fr'): ?>
           Adresse du traducteur : VYBmQtx6o7AxPWjLWNywk58rKmUXKyqRyD<br>
