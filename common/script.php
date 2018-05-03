06e289adbf72ac40268fa40f50dfe18e08c0759c
vips-web <tiger4thv@gmail.com>
2018-05-04 00:21:54 +0900

    イベント画像表示

--- common/script.php
+++ common/script.php
@@ -13,3 +13,23 @@
 <script type="text/javascript" src="<?php echo URL; ?>/js/jquery.inview.min.js"></script>
 <script type="text/javascript" src="<?php echo URL; ?>/js/main.js"></script>
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5ab60eab9e6f635f"></script>
+
+<?php if ($lang == 'en'): ?>
+  <script type="text/javascript" src="<?php echo URL; ?>/js/lightbox.min.js"></script>
+  <a href="<?php echo URL; ?>/images/airdrop-en.png"
+     class="js_event"
+     data-lightbox="event"
+     data-title="AIRDROP EVENT<br><a href='https://twitter.com/VIPSTARCOIN' target='_blank'>MORE INFORMATION <i class='fas fa-external-link-alt'></i></a>"></a>
+<?php elseif ($lang == 'de'): ?>
+  <script type="text/javascript" src="<?php echo URL; ?>/js/lightbox.min.js"></script>
+  <a href="<?php echo URL; ?>/images/airdrop-de.png"
+     class="js_event"
+     data-lightbox="event"
+     data-title="AIRDROP EVENT<br><a href='https://twitter.com/VIPSTARCOIN' target='_blank'>MORE INFORMATION <i class='fas fa-external-link-alt'></i></a>"></a>
+<?php elseif ($lang == 'fr'): ?>
+  <script type="text/javascript" src="<?php echo URL; ?>/js/lightbox.min.js"></script>
+  <a href="<?php echo URL; ?>/images/airdrop-fr.png"
+     class="js_event"
+     data-lightbox="event"
+     data-title="AIRDROP EVENT<br><a href='https://twitter.com/VIPSTARCOIN' target='_blank'>MORE INFORMATION <i class='fas fa-external-link-alt'></i></a>"></a>
+<?php endif; ?>
