4ebf2710d2976776d9692b69f9da07b5ea0de8a7
vips-web <tiger4thv@gmail.com>
2018-05-25 00:44:54 +0900

    エアドロップ案内削除

--- common/script.php
+++ common/script.php
@@ -13,11 +13,3 @@
 <script type="text/javascript" src="<?php echo URL; ?>/js/jquery.inview.min.js"></script>
 <script type="text/javascript" src="<?php echo URL; ?>/js/main.js"></script>
 <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5ab60eab9e6f635f"></script>
-
-<?php if ($lang == 'en'): ?>
-  <script type="text/javascript" src="<?php echo URL; ?>/js/lightbox.min.js"></script>
-  <a href="<?php echo URL; ?>/images/airdrop-en.png"
-     class="js_event"
-     data-lightbox="event"
-     data-title="Please join <a href='https://discord.gg/sEaaxau' target='_blank'>Discord <i class='fas fa-external-link-alt'></i></a> and see #air-drop-instruction."></a>
-<?php endif; ?>
