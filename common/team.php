bc148fdccef80d67ec8189f891f9d1fa9028c97d
vips-web <tiger4thv@gmail.com>
2018-05-28 00:17:06 +0900

    team追加

---
+++ common/team.php
@@ -0,0 +1,59 @@
+<div id="team" class="text-center">
+  <div class="container">
+    <div class="section-title center">
+      <h2>TEAM</h2>
+      <hr>
+    </div>
+    <?php if ($lang == 'ja' || $lang == 'vip'): ?>
+      <div class="row col-md-10 col-md-offset-1">
+        <div class="col-md-1"></div>
+        <div class="col-md-2">
+          <img src="<?php echo URL; ?>/img/team/boon.png" class="" alt="">
+          <p class="team-name">VIP☆STAR<br>（ブーン）</p>
+        </div>
+        <div class="col-md-2">
+          <img src="<?php echo URL; ?>/img/team/takuzou.png" class="" alt="">
+          <p class="team-name">たくぞう</p>
+        </div>
+        <div class="col-md-2">
+          <img src="<?php echo URL; ?>/img/team/uruhu.png" class="" alt="">
+          <p class="team-name">uruhu</p>
+        </div>
+        <div class="col-md-2">
+          <img src="<?php echo URL; ?>/img/team/orute.png" class="" alt="">
+          <p class="team-name">orute</p>
+        </div>
+        <div class="col-md-2">
+          <img src="<?php echo URL; ?>/img/team/m.png" class="" alt="">
+          <p class="team-name">寺生まれのM</p>
+        </div>
+        <div class="col-md-1"></div>
+      </div>
+    <?php else: ?>
+      <div class="row col-md-10 col-md-offset-1">
+        <div class="col-md-1"></div>
+        <div class="col-md-2">
+          <img src="<?php echo URL; ?>/img/team/boon.png" class="" alt="VIP☆STAR">
+          <p class="team-name">VIP☆STAR</p>
+        </div>
+        <div class="col-md-2">
+          <img src="<?php echo URL; ?>/img/team/takuzou.png" class="" alt="takuzou">
+          <p class="team-name">takuzou</p>
+        </div>
+        <div class="col-md-2">
+          <img src="<?php echo URL; ?>/img/team/uruhu.png" class="" alt="uruhu">
+          <p class="team-name">uruhu</p>
+        </div>
+        <div class="col-md-2">
+          <img src="<?php echo URL; ?>/img/team/orute.png" class="" alt="orute">
+          <p class="team-name">orute</p>
+        </div>
+        <div class="col-md-2">
+          <img src="<?php echo URL; ?>/img/team/m.png" class="" alt="M">
+          <p class="team-name">M</p>
+        </div>
+        <div class="col-md-1"></div>
+      </div>
+    <?php endif; ?>
+  </div>
+</div>
