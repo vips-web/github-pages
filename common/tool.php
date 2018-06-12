d669e59f40ca267752458a3e23de1c1ad702024b
vips-web <tiger4thv@gmail.com>
2018-06-13 00:31:00 +0900

    RegVIPS, QRVips追加

--- common/tool.php
+++ common/tool.php
@@ -7,22 +7,66 @@
     <div class="row">
       <div class="col-md-8 col-md-offset-2">
         <div class="tool-button">
-          <a href="https://github.com/vips-web/github-pages/raw/master/download/MiningHelper.zip" class="button button-bw" target="_blank">
-            Mining Helper (Win)&nbsp;&nbsp;<i class="fas fa-file-archive"></i>
+          <a class="button button-bw" href="#" data-toggle="modal" data-target="#tippingModal">
+            TIPPING TOOL&nbsp;&nbsp;<i class="far fa-window-restore"></i>
           </a><br>
-          <a href="https://github.com/vips-web/github-pages/raw/master/download/MiningHelper.dmg" class="button button-bw" target="_blank">
-            Mining Helper (Mac)&nbsp;&nbsp;<i class="fas fa-file-archive"></i>
-          </a><br>
-          <a href="https://github.com/vips-web/github-pages/raw/master/download/ccminer.zip" class="button button-bw" target="_blank">
-            CCminer&nbsp;&nbsp;<i class="fas fa-file-archive"></i>
-          </a><br>
-          <br><br>
-          <a href="https://fkfk.github.io/vips/qrcode/" class="button button-bw" target="_blank">
-            QRCode Generator&nbsp;&nbsp;<i class="fas fa-external-link-alt"></i>
+          <a class="button button-bw" href="#" data-toggle="modal" data-target="#miningModal">
+            MINING TOOL&nbsp;&nbsp;<i class="far fa-window-restore"></i>
           </a><br>
           <a href="http://explorer.vipstarcoin.jp/" class="button button-bw" target="_blank">
             VIPSTARCOIN Explorer&nbsp;&nbsp;<i class="fas fa-external-link-alt"></i>
           </a>
+        </div>
+      </div>
+    </div>
+    <div class="modal fade" id="tippingModal" tabindex="-1" role="dialog" aria-labelledby="tippingModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="tippingModalLabel">TIPPING TOOL</h4>
+          </div>
+          <div class="modal-body">
+            <a href="https://fkfk.github.io/vips/qrcode/" class="button button-bw" target="_blank">
+              QRCode Generator&nbsp;&nbsp;<i class="fas fa-external-link-alt"></i>
+            </a><br>
+            <a href="https://github.com/vips-web/github-pages/raw/master/download/QRVipsWin.zip" class="button button-bw" target="_blank">
+              QRVIPS (Win)&nbsp;&nbsp;<i class="fas fa-file-archive"></i>
+            </a><br>
+            <a href="https://github.com/vips-web/github-pages/raw/master/download/QRVipsMac.dmg" class="button button-bw" target="_blank">
+              QRVIPS (Mac)&nbsp;&nbsp;<i class="fas fa-file-archive"></i>
+            </a><br>
+            <a href="https://github.com/vips-web/github-pages/raw/master/download/RegVIPS.zip" class="button button-bw" target="_blank">
+              RegVIPS (Win)&nbsp;&nbsp;<i class="fas fa-file-archive"></i>
+            </a>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="button button-bw" data-dismiss="modal">CLOSE</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="modal fade" id="miningModal" tabindex="-1" role="dialog" aria-labelledby="miningModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="miningModalLabel">MINING TOOL</h4>
+          </div>
+          <div class="modal-body">
+            <a href="https://github.com/vips-web/github-pages/raw/master/download/MiningHelper.zip" class="button button-bw" target="_blank">
+              Mining Helper (Win)&nbsp;&nbsp;<i class="fas fa-file-archive"></i>
+            </a><br>
+            <a href="https://github.com/vips-web/github-pages/raw/master/download/MiningHelper.dmg" class="button button-bw" target="_blank">
+              Mining Helper (Mac)&nbsp;&nbsp;<i class="fas fa-file-archive"></i>
+            </a><br>
+            <a href="https://github.com/vips-web/github-pages/raw/master/download/ccminer.zip" class="button button-bw" target="_blank">
+              CCminer&nbsp;&nbsp;<i class="fas fa-file-archive"></i>
+            </a>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="button button-bw" data-dismiss="modal">CLOSE</button>
+          </div>
         </div>
       </div>
     </div>
