903bd1d8c3000fb16cc198e3e046d6888cf61b77
vips-web <tiger4thv@gmail.com>
2018-07-01 22:39:01 +0900

    たくぞうマウスオーバーで実写化

--- common/team.php
+++ common/team.php
@@ -1,55 +1,62 @@
 <div id="team" class="text-center">
   <div class="container">
-    <div class="section-title center">
-      <h2>TEAM</h2>
-      <hr>
-    </div>
+
     <?php if ($lang == 'ja' || $lang == 'vip'): ?>
-      <div class="row col-md-10 col-md-offset-1">
+      <div class="section-title center">
+        <h2>TEAM</h2>
+        <hr>
+        <h3>株式会社VIPSTAR</h3>
+      </div>
+      <div class="row col-md-10 col-md-offset-1" ontouchstart="">
         <div class="col-md-1"></div>
         <div class="col-md-2">
-          <img src="<?php echo URL; ?>/img/team/boon.png" alt="VIP☆STAR">
-          <p class="team-name">VIP☆STAR<br>（ブーン）</p>
+          <p class="team-image team-boon"></p>
+          <p class="team-name">VIP☆STAR<br>（ブーン）<br><br>代表取締役</p>
         </div>
         <div class="col-md-2">
-          <img src="<?php echo URL; ?>/img/team/takuzou.png" alt="たくぞう">
-          <p class="team-name">たくぞう</p>
+          <p class="team-image team-takuzo"></p>
+          <p class="team-name">吉澤拓哉<br>（たくぞう）<br><br>Chief Marketing Officer</p>
         </div>
         <div class="col-md-2">
-          <img src="<?php echo URL; ?>/img/team/uruhu.png" alt="uruhu">
+          <p class="team-image team-uruhu"></p>
           <p class="team-name">uruhu</p>
         </div>
         <div class="col-md-2">
-          <img src="<?php echo URL; ?>/img/team/orute.png" alt="orute">
+          <p class="team-image team-orute"></p>
           <p class="team-name">orute</p>
         </div>
         <div class="col-md-2">
-          <img src="<?php echo URL; ?>/img/team/m.png" alt="寺生まれのM">
+          <p class="team-image team-m"></p>
           <p class="team-name">寺生まれのM</p>
         </div>
         <div class="col-md-1"></div>
       </div>
     <?php else: ?>
-      <div class="row col-md-10 col-md-offset-1">
+      <div class="section-title center">
+        <h2>TEAM</h2>
+        <hr>
+        <h3>VIPSTAR.Co</h3>
+      </div>
+      <div class="row col-md-10 col-md-offset-1" ontouchstart="">
         <div class="col-md-1"></div>
         <div class="col-md-2">
-          <img src="<?php echo URL; ?>/img/team/boon.png" alt="VIP☆STAR">
-          <p class="team-name">VIP☆STAR<br>(Boon)</p>
+          <p class="team-image team-boon"></p>
+          <p class="team-name">VIP☆STAR<br>（Boon）<br><br><br>Representative Director</p>
         </div>
         <div class="col-md-2">
-          <img src="<?php echo URL; ?>/img/team/takuzou.png" alt="takuzou">
-          <p class="team-name">takuzou</p>
+          <p class="team-image team-takuzo"></p>
+          <p class="team-name">Takuya Yoshizawa<br>（takuzo）<br><br>Chief Marketing Officer</p>
         </div>
         <div class="col-md-2">
-          <img src="<?php echo URL; ?>/img/team/uruhu.png" alt="uruhu">
+          <p class="team-image team-uruhu"></p>
           <p class="team-name">uruhu</p>
         </div>
         <div class="col-md-2">
-          <img src="<?php echo URL; ?>/img/team/orute.png" alt="orute">
+          <p class="team-image team-orute"></p>
           <p class="team-name">orute</p>
         </div>
         <div class="col-md-2">
-          <img src="<?php echo URL; ?>/img/team/m.png" alt="M">
+          <p class="team-image team-m"></p>
           <p class="team-name">M</p>
         </div>
         <div class="col-md-1"></div>
