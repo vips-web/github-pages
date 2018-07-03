b2664049040acbb78b596095d1c8deab37b1716a
vips-web <tiger4thv@gmail.com>
2018-07-03 22:40:53 +0900

    team, supporterのマージンが潰れないように修正

--- common/supporter.php
+++ common/supporter.php
@@ -5,61 +5,53 @@
       <hr>
     </div>
     <?php if ($lang == 'ja' || $lang == 'vip'): ?>
-      <div class="row col-md-10 col-md-offset-1">
-        <div class="col-md-1"></div>
-        <div class="col-md-2"></div>
-        <div class="col-md-2">
+      <div class="supporter-row">
+        <div>
           <a href="https://minori.co/" class="supporter-link" target="_blank">
             <img src="<?php echo URL; ?>/img/supporter/minori_s.png" alt="minori">
             <p class="supporter-name">minori</p>
           </a>
         </div>
-        <div class="col-md-2">
+        <div>
           <a href="https://www.shironoyuki.com/" class="supporter-link" target="_blank">
             <img src="<?php echo URL; ?>/img/supporter/Shirono_Yuki_s.png" alt="白乃雪">
             <p class="supporter-name">白乃雪</p>
           </a>
         </div>
-        <div class="col-md-2">
+        <div>
           <a href="https://nightmaker.tictail.com/" class="supporter-link" target="_blank">
             <img src="<?php echo URL; ?>/img/supporter/Nightmaker_s.png" alt="Nightmaker">
             <p class="supporter-name">Nightmaker</p>
           </a>
         </div>
-        <div class="col-md-2"></div>
-        <div class="col-md-1"></div>
       </div>
-      <div class="bl_partner-button col-md-12">
+      <div>
         <a class="button button-bw" href="<?php echo URL; ?>/supporter/">
           サポーター詳細&nbsp;&nbsp;<i class="fas fa-angle-right"></i>
         </a>
       </div>
     <?php else: ?>
-      <div class="row col-md-10 col-md-offset-1">
-        <div class="col-md-1"></div>
-        <div class="col-md-2"></div>
-        <div class="col-md-2">
+      <div class="supporter-row">
+        <div>
           <a href="https://minori.co/" class="supporter-link" target="_blank">
             <img src="<?php echo URL; ?>/img/supporter/minori_s.png" alt="minori">
             <p class="supporter-name">minori</p>
           </a>
         </div>
-        <div class="col-md-2">
+        <div>
           <a href="https://www.shironoyuki.com/" class="supporter-link" target="_blank">
             <img src="<?php echo URL; ?>/img/supporter/Shirono_Yuki_s.png" alt="Shirono Yuki">
             <p class="supporter-name">Shirono Yuki</p>
           </a>
         </div>
-        <div class="col-md-2">
+        <div>
           <a href="http://nightmaker.tictail.com/" class="supporter-link" target="_blank">
             <img src="<?php echo URL; ?>/img/supporter/Nightmaker_s.png" alt="Nightmaker">
             <p class="supporter-name">Nightmaker</p>
           </a>
         </div>
-        <div class="col-md-2"></div>
-        <div class="col-md-1"></div>
       </div>
-      <div class="bl_partner-button col-md-12">
+      <div>
         <a class="button button-bw" href="<?php echo URL; ?>/supporter/">
           SUPPORTERS DETAIL&nbsp;&nbsp;<i class="fas fa-angle-right"></i>
         </a>
--- common/team.php
+++ common/team.php
@@ -1,35 +1,32 @@
 <div id="team" class="text-center">
   <div class="container">
-
     <?php if ($lang == 'ja' || $lang == 'vip'): ?>
       <div class="section-title center">
         <h2>TEAM</h2>
         <hr>
         <h3>株式会社VIPSTAR</h3>
       </div>
-      <div class="row col-md-10 col-md-offset-1" ontouchstart="">
-        <div class="col-md-1"></div>
-        <div class="col-md-2">
+      <div class="team-row" ontouchstart="">
+        <div>
           <p class="team-image team-boon"></p>
           <p class="team-name">VIP☆STAR<br>（ブーン）<br><br>代表取締役</p>
         </div>
-        <div class="col-md-2">
+        <div>
           <p class="team-image team-takuzo"></p>
           <p class="team-name">吉澤拓哉<br>（たくぞう）<br><br>Chief Marketing Officer</p>
         </div>
-        <div class="col-md-2">
+        <div>
           <p class="team-image team-uruhu"></p>
           <p class="team-name">uruhu</p>
         </div>
-        <div class="col-md-2">
+        <div>
           <p class="team-image team-orute"></p>
           <p class="team-name">orute</p>
         </div>
-        <div class="col-md-2">
+        <div>
           <p class="team-image team-m"></p>
           <p class="team-name">寺生まれのM</p>
         </div>
-        <div class="col-md-1"></div>
       </div>
     <?php else: ?>
       <div class="section-title center">
@@ -37,29 +34,27 @@
         <hr>
         <h3>VIPSTAR.Co</h3>
       </div>
-      <div class="row col-md-10 col-md-offset-1" ontouchstart="">
-        <div class="col-md-1"></div>
-        <div class="col-md-2">
+      <div class="team-row" ontouchstart="">
+        <div>
           <p class="team-image team-boon"></p>
           <p class="team-name">VIP☆STAR<br>（Boon）<br><br><br>Representative Director</p>
         </div>
-        <div class="col-md-2">
+        <div>
           <p class="team-image team-takuzo"></p>
           <p class="team-name">Takuya Yoshizawa<br>（takuzo）<br><br>Chief Marketing Officer</p>
         </div>
-        <div class="col-md-2">
+        <div>
           <p class="team-image team-uruhu"></p>
           <p class="team-name">uruhu</p>
         </div>
-        <div class="col-md-2">
+        <div>
           <p class="team-image team-orute"></p>
           <p class="team-name">orute</p>
         </div>
-        <div class="col-md-2">
+        <div>
           <p class="team-image team-m"></p>
           <p class="team-name">M</p>
         </div>
-        <div class="col-md-1"></div>
       </div>
     <?php endif; ?>
   </div>
