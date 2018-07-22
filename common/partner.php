0789cdd3951b33bab408cc937ff517a6b104c9b1
vips-web <tiger4thv@gmail.com>
2018-07-22 16:21:21 +0900

    partner追加

--- common/partner.php
+++ common/partner.php
@@ -4,8 +4,15 @@
     <hr>
   </div>
   <div class="partner-image">
-    <a href="https://nzigen.com/" target="_blank">
-      <img src="<?php echo URL; ?>/img/partner/nzigen.png" alt="エヌ次元" width="300">
-    </a>
+    <div>
+      <a href="https://nzigen.com/" target="_blank">
+        <img src="<?php echo URL; ?>/img/partner/nzigen.png" alt="エヌ次元" width="320">
+      </a>
+    </div>
+    <div>
+      <a href="http://www.aliengames.co.jp/" target="_blank">
+        <img src="<?php echo URL; ?>/img/partner/aliengames.png" alt="AlienGames" width="240">
+      </a>
+    </div>
   </div>
 </div>
