8661accba5ea058c69a28fdcfe135d6626d32982
vips-web <tiger4thv@gmail.com>
2018-08-13 23:53:34 +0900

    トップリンク色変更

--- common/special.php
+++ common/special.php
@@ -8,7 +8,7 @@
 <div style="text-align: center; margin-top: 100px;">
   <a href="<?php echo $special_url; ?>" 
      class="btn btn-warning btn-lg" 
-     style="width: 250px; height: 60px; padding: 14px 16px; font-size: 22px; font-weight: bold; background-color: #ffc000; border-color: #ffc000;">
+     style="width: 250px; height: 60px; padding: 14px 16px; font-size: 22px; font-weight: bold; background: linear-gradient(to right, #30cfd0, #330867); border-color: #fff;">
     IAB@gamescom&nbsp;&nbsp;<i class="fas fa-angle-right"></i>
   </a>
 </div>
