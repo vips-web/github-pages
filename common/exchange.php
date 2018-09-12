ff443db738f6bae044263255356a5f34073da485
vips-web <tiger4thv@gmail.com>
2018-09-13 00:02:02 +0900

    exchangeにtopbtc追加

--- common/exchange.php
+++ common/exchange.php
@@ -4,14 +4,17 @@
     <hr>
   </div>
   <div class="exchange-image">
-    <a href="https://www.coinexchange.io/market/VIPS/BTC" target="_blank">
-      <img src="<?php echo URL; ?>/img/exchange/coinexchange.png" alt="CoinExchange" width="400">
+    <a href="https://topbtc.com/home/Market/index/market/USDT/coin/VIPS.html" target="_blank" class="exchange-topbtc">
+      <img src="<?php echo URL; ?>/img/exchange/topbtc.png" alt="TOPBTC">
     </a><br>
-    <a href="https://wallet.crypto-bridge.org/market/BRIDGE.VIPS_BRIDGE.BTC" target="_blank">
-      <img src="<?php echo URL; ?>/img/exchange/cryptobridge.svg" alt="CryptoBridge" width="250">
+    <a href="https://www.coinexchange.io/market/VIPS/BTC" target="_blank" class="exchange-coinexchange">
+      <img src="<?php echo URL; ?>/img/exchange/coinexchange.png" alt="CoinExchange">
     </a><br>
-    <a href="https://www.coinexmarket.io" target="_blank">
-      <img src="<?php echo URL; ?>/img/exchange/coinexmarket.png" alt="CoinExMarket" width="250">
+    <a href="https://wallet.crypto-bridge.org/market/BRIDGE.VIPS_BRIDGE.BTC" target="_blank" class="exchange-cryptobridge">
+      <img src="<?php echo URL; ?>/img/exchange/cryptobridge.svg" alt="CryptoBridge">
+    </a><br>
+    <a href="https://www.coinexmarket.io" target="_blank" class="exchange-coinexmarket">
+      <img src="<?php echo URL; ?>/img/exchange/coinexmarket.png" alt="CoinExMarket">
     </a><br>
     <?php if ($lang == 'ja' || $lang == 'vip'): ?>
       <br><br><br>
