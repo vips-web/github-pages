b0bff58329c1917f5fadf4e36250503268f0d041
vips-web <vips-web@gmail.com>
2019-08-06 21:19:33 +0900

    news一部更新

--- common/special.php
+++ common/special.php
@@ -7,7 +7,8 @@
 ?>
 
 <div style="text-align: center; margin-top: 100px;">
-  <a href="<?php echo $special_url; ?>" 
+  <a href="<?php echo $special_url; ?>"
+     target="_blank"
      class="btn btn-warning btn-lg" 
      style="max-width: 90%; min-height: 60px; padding: 14px 16px; font-size: 22px; font-weight: bold; background: linear-gradient(to right,#999900,#009900); border-color: #fff;">
      アシャンティ王国の<br>公式仮想通貨に認定<br>詳細はこちら&nbsp;&nbsp;<i class="fas fa-angle-right"></i>
