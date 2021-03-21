a370abc09fdd4f6afedf72056ef8a04ca54f5613
vips-web <vips-web@gmail.com>
2021-03-21 15:39:56 +0900

    topics修正

--- common/topics.php
+++ common/topics.php
@@ -11,16 +11,16 @@
             <?php echo ($lang == 'ja' || $lang == 'vip') ? '株式会社VIPSTAR 公式サイト' : 'VIPSTAR Inc. Website'; ?>
             &nbsp;<i class="fas fa-external-link-alt"></i>
           </a><br>
+          <a href="https://discord.gg/vJz35a5EWP" class="button button-bw topics-button-discord" target="_blank">
+            Discord
+            &nbsp;<i class="fas fa-external-link-alt"></i>
+          </a><br>
           <a href="https://discord.gg/387jRwC" class="button button-bw topics-button-auction" target="_blank">
             <?php echo ($lang == 'ja' || $lang == 'vip') ? 'VIPSフリマ、オークション' : 'VIPS Free Market & Auction'; ?>
             &nbsp;<i class="fas fa-external-link-alt"></i>
           </a><br>
           <a href="https://vips-blog.hatenablog.com/about" class="button button-bw topics-button-blog" target="_blank">
             <?php echo ($lang == 'ja' || $lang == 'vip') ? 'VIPSブログ' : 'VIPS Blog'; ?>
-            &nbsp;<i class="fas fa-external-link-alt"></i>
-          </a><br>
-          <a href="https://www.vipstarcoin.jp/game/moyashiproject/" class="button button-bw topics-button-moyashi" target="_blank">
-            <?php echo ($lang == 'ja' || $lang == 'vip') ? 'ゆぐもやしる（サービス終了）' : 'Yugumoyashiru (no longer available)'; ?>
             &nbsp;<i class="fas fa-external-link-alt"></i>
           </a>
         </div>
