a63668ac2067483c0dfbe473dca5d98bc0e212e5
vips-web <vips-web@gmail.com>
2021-03-21 16:04:34 +0900

    5chのリンク追加

--- common/contact.php
+++ common/contact.php
@@ -39,6 +39,9 @@
           <li><a href="https://discord.gg/vJz35a5EWP" target="_blank">
             <i class="fab fa-discord fa-4x"></i></i>
           </a></li>
+          <li><a href="http://find.5ch.net/search?q=VIPS" target="_blank">
+            <i class="social-5ch">5</i>
+          </a></li>
           <li><a href="https://twitter.com/<?php echo $twitter_account; ?>" target="_blank">
             <i class="fab fa-twitter-square fa-4x"></i></i>
           </a></li>
