b052b224d0460155713ec8bf31443ced2a07b950
vips-web <vips-web@gmail.com>
2021-06-06 15:32:04 +0900

    exchange整理

--- common/exchange.php
+++ common/exchange.php
@@ -10,15 +10,6 @@
     <a href="https://topbtc.com/home/Market/index/market/USDT/coin/VIPS.html" target="_blank" class="exchange-topbtc">
       <img src="<?php echo URL; ?>/img/exchange/topbtc.png" alt="TOPBTC">
     </a><br>
-    <a href="https://www.swft.pro/" target="_blank" class="exchange-swft-blockchain">
-      <img src="<?php echo URL; ?>/img/exchange/swft-blockchain.jpg" alt="SWFT Blockchain">
-    </a><br>
-    <a href="https://www.cryptaldash.com/" target="_blank" class="exchange-cryptaldash">
-      <img src="<?php echo URL; ?>/img/exchange/cryptaldash.png" alt="CryptalDash">
-    </a><br>
-    <a href="https://ataix.com/currencies/VIPS" target="_blank" class="exchange-ataix">
-      <img src="<?php echo URL; ?>/img/exchange/ataix.svg" alt="ATAIX">
-    </a><br>
     <a href="https://tradecoin.space/" target="_blank" class="exchange-treco">
       <img src="<?php echo URL; ?>/img/exchange/treco.png" alt="TRECO">
     </a><br>
